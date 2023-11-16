fc02498e18760c3dcd4893b3e2af3cfe7de845c8
maxjlaz1 <maxjlaz@icloud.com>
2023-11-16 16:39:08 -0500

    fixed static issue

--- Leviathan/Management.hpp
+++ Leviathan/Management.hpp
@@ -3,7 +3,7 @@
 #ifndef FILE_H
 #define FILE_H
 
-static enum LEV_CODE { CLEAR, CONN_ERROR, TEST };
+enum LEV_CODE { CLEAR, CONN_ERROR, TEST };
 
 static std::string CodeDef(LEV_CODE code) {
 	if (code == LEV_CODE::CLEAR)
