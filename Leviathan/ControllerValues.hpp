944699ac467db3d5e42433efabede4879557dac4
RPI5 <maxjlaz8@gmail.com>
2024-03-08 21:34:29 -0500

    Strings for controller vals

--- Leviathan/ControllerValues.hpp
+++ Leviathan/ControllerValues.hpp
@@ -10,8 +10,8 @@ public:
 
     std::string toString()
     {
-        return ljoyx + "!" + ljoyy + "!" + rjoyx + "!" + rjoyy + "!" + ltrigger + "!" + rtrigger + "!"
-         + a + "!" + b + "!" + x + "!" + y + "!" + lbumper + "!" + rbumper + "!" + up + "!" + down + "!" + left + "!" + right;
+        return std::to_string(ljoyx) + "!" + std::to_string(ljoyy) + "!" + std::to_string(rjoyx) + "!" + std::to_string(rjoyy) + "!" + std::to_string(ltrigger) + "!" + std::to_string(rtrigger) + "!"
+         + std::to_string(a) + "!" + std::to_string(b) + "!" + std::to_string(x) + "!" + std::to_string(y) + "!" + std::to_string(lbumper) + "!" + std::to_string(rbumper) + "!" + std::to_string(up) + "!" + std::to_string(down) + "!" + std::to_string(left) + "!" + std::to_string(right);
     }
 };
 
