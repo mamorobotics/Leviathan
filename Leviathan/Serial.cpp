f7936a7dc0675a0013fb800c50f2761b00cdda87
RPI5 <maxjlaz8@gmail.com>
2024-09-07 18:52:26 -0400

    added serial send delay

--- Leviathan/Serial.cpp
+++ Leviathan/Serial.cpp
@@ -6,9 +6,21 @@ void Serial::SendController()
 	while(true){
 		ControllerValues* controllerValues = controller->GetControllerValues();
 		//std::cout<<controllerValues->toString().c_str()<<std::endl;
-		const char* msgSerial = controllerValues->toString().c_str();
+		std::string msgStr = "<" + controllerValues->toString() + ">";
 
-		RS232_cputs(cport_nr, msgSerial); // sends string on serial
+		// unsigned char* msg = new unsigned char[msgStr.size() + 1];
+		// std::memcpy(msg, msgStr.c_str(), msgStr.size()+1);
+
+		// std::cout<<msg<<std::endl;
+
+		// int err = RS232_SendBuf(cport_nr, msg, msgStr.size() + 1); // sends string on serial
+		// if(err == -1){
+		// 	std::cout<<"failed send" << std::endl;
+		// }
+
+		RS232_cputs(cport_nr, msgStr.c_str());
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
 		//printf("Sent to Arduino: '%s'\n", msgSerial);
 	}
--- Leviathan/Serial.hpp
+++ Leviathan/Serial.hpp
@@ -1,24 +1,27 @@
 #pragma once
-// #include <iostream>
-// #include <tuple>
-// #include <sstream>
-// #include <thread>
-// #include <opencv2/opencv.hpp>
+#include <iostream>
+#include <tuple>
+#include <sstream>
+#include <thread>
+#include <opencv2/opencv.hpp>
 
-// #include <asio.hpp>
+#include <asio.hpp>
 #include "UI.hpp"
-// #include <unistd.h>
-// #include <atomic>
-// #include <thread>
+#include <unistd.h>
+#include <atomic>
+#include <thread>
+#include <chrono>
 
-// #include "glad/glad.h"
-// #include "GLFW/glfw3.h"
+#include "Management.hpp"
+
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
 #include "lib/RS-232/rs232.h"
 
 class Serial
 {
 private:
-	int cport_nr = 24; //GET PORT USING COMMANDS FROM https://www.monocilindro.com/2017/02/19/how-to-connect-arduino-and-raspberry-pi-using-usb-and-c/
+	int cport_nr = 16; //GET PORT USING COMMANDS FROM https://www.monocilindro.com/2017/02/19/how-to-connect-arduino-and-raspberry-pi-using-usb-and-c/
 
 	static Serial* serial;
 
@@ -29,7 +32,9 @@ public:
 
 		if(RS232_OpenComport(cport_nr, bdrate, mode, 0))
         {
-            printf("Can not open comport\n");
+			//UI* gui = UI::Get();
+            //gui->PublishOutput("Can not open comport", LEV_CODE::CONN_ERROR);
+			std::cout<<"cannnot open comport"<<std::endl;
         }
 	}
 	void SendController();
